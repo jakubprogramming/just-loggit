1d88d784a49b398c00fce336595ba3f636ea18b3
work <work@Jakubs-MacBook-Pro.local>
2021-09-10 10:31:22 +0200

    run standard --fix

--- logger.js
+++ logger.js
@@ -1,114 +1,109 @@
-require('dotenv').config();
-const winston = require('winston');
-const {format, createLogger, transports} = winston;
-const {timestamp, combine, printf, colorize, errors} = format;
+require('dotenv').config()
+const winston = require('winston')
+const { format, createLogger, transports } = winston
+const { timestamp, combine, printf, colorize, errors } = format
 
-if(process.env.LOGGING_TO_DB_ENABLED === "true"){
-
-  if(!process.env.LOGGING_MONGO_DB_USER ||
+if (process.env.LOGGING_TO_DB_ENABLED === 'true') {
+  if (!process.env.LOGGING_MONGO_DB_USER ||
      !process.env.LOGGING_MONGO_DB_PW ||
      !process.env.LOGGING_MONGO_DB_URL ||
      !process.env.LOGGING_MONGO_DB_REPLICA_SET ||
      !process.env.LOGGING_MONGO_ERROR_DB ||
      !process.env.LOGGING_MONGO_ERROR_COLLECTION
-   ) throw("Incomplete MongoDB environment variables. Please check your .env file");
+  ) throw ('Incomplete MongoDB environment variables. Please check your .env file')
 
-  require('winston-mongodb');
-  var MongoClient = require('mongodb').MongoClient;
-  var uri = `mongodb://${process.env.LOGGING_MONGO_DB_USER}:${process.env.LOGGING_MONGO_DB_PW}@${process.env.LOGGING_MONGO_DB_URL}/${process.env.LOGGING_MONGO_ERROR_DB}`;
+  require('winston-mongodb')
+  const MongoClient = require('mongodb').MongoClient
+  const uri = `mongodb://${process.env.LOGGING_MONGO_DB_USER}:${process.env.LOGGING_MONGO_DB_PW}@${process.env.LOGGING_MONGO_DB_URL}/${process.env.LOGGING_MONGO_ERROR_DB}`
   var client = new MongoClient(uri, {
     useUnifiedTopology: true,
     ssl: true,
     replicaSet: process.env.LOGGING_MONGO_DB_REPLICA_SET,
     authSource: 'admin',
     retryWrites: true
-  });
-  var connection = client.connect();
+  })
+  const connection = client.connect()
 }
 
 const logPrintf = printf(({ level, message, timestamp, stack }) => {
-  return `${timestamp} ${level}: ${stack || message}`;
-});
+  return `${timestamp} ${level}: ${stack || message}`
+})
 
 const devLogFormat = combine(
-  timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
-  errors({stack: true}),
+  timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+  errors({ stack: true }),
   logPrintf,
   format.metadata()
-);
+)
 
 const prodLogFormat = combine(
-  timestamp({format: 'YYYY-MM-DD HH:mm:ss ZZ'}),
-  errors({stack: true}),
+  timestamp({ format: 'YYYY-MM-DD HH:mm:ss ZZ' }),
+  errors({ stack: true }),
   logPrintf,
   format.metadata()
-);
+)
 
-const appliedFormat = process.env.NODE_ENV === "development" ? devLogFormat : prodLogFormat;
+const appliedFormat = process.env.NODE_ENV === 'development' ? devLogFormat : prodLogFormat
 
 const myTransports = [
-  new transports.Console({format: combine(colorize(), appliedFormat)}),
+  new transports.Console({ format: combine(colorize(), appliedFormat) }),
   new transports.File({ filename: `${process.env.DIRECTORY_LOGFILES || './logs'}/error.log`, level: 'error' }),
   new transports.File({ filename: `${process.env.DIRECTORY_LOGFILES || './logs'}/combined.log` })
-];
+]
 
-if(process.env.LOGGING_TO_DB_ENABLED === "true"){
+if (process.env.LOGGING_TO_DB_ENABLED === 'true') {
   myTransports.push(winston.add(new winston.transports.MongoDB({
-      level: 'error',
-      db: client,
-      collection: process.env.LOGGING_MONGO_ERROR_COLLECTION,
-      storeHost: true,
-      leaveConnectionOpen: true,
-      decolorize: true,
-    })));
+    level: 'error',
+    db: client,
+    collection: process.env.LOGGING_MONGO_ERROR_COLLECTION,
+    storeHost: true,
+    leaveConnectionOpen: true,
+    decolorize: true
+  })))
 }
 
 const logger = createLogger({
-  level: process.env.LOGGER_LEVEL || "debug",
+  level: process.env.LOGGER_LEVEL || 'debug',
   format: appliedFormat,
   defaultMeta: { NODE_ENV: process.env.NODE_ENV },
   transports: myTransports
-});
+})
 
-
-const wrapper = ( original ) => {
-    return (...args) => {
-
-      if(args.length === 1){
-        return original(
-          typeof args[0] === "object" ? JSON.stringify(args[0], null, 2) : args[0]
-        )
-      }
-
+const wrapper = (original) => {
+  return (...args) => {
+    if (args.length === 1) {
       return original(
-        args.reduce((a, b) => {
-
-          if(typeof a === "object") a = JSON.stringify(a, null, 2);
-          if(typeof b === "object") b = JSON.stringify(b, null, 2);
-
-          return a + " " + b;
-        }));
+        typeof args[0] === 'object' ? JSON.stringify(args[0], null, 2) : args[0]
+      )
     }
-};
 
-logger.error = wrapper(logger.error);
-logger.warn = wrapper(logger.warn);
-logger.info = wrapper(logger.info);
-logger.verbose = wrapper(logger.verbose);
-logger.debug = wrapper(logger.debug);
-logger.silly = wrapper(logger.silly);
+    return original(
+      args.reduce((a, b) => {
+        if (typeof a === 'object') a = JSON.stringify(a, null, 2)
+        if (typeof b === 'object') b = JSON.stringify(b, null, 2)
 
+        return a + ' ' + b
+      }))
+  }
+}
 
-module.exports = logger;
+logger.error = wrapper(logger.error)
+logger.warn = wrapper(logger.warn)
+logger.info = wrapper(logger.info)
+logger.verbose = wrapper(logger.verbose)
+logger.debug = wrapper(logger.debug)
+logger.silly = wrapper(logger.silly)
+
+module.exports = logger
 
 process.on('exit', (code) => {
-  cleanup();
-});
+  cleanup()
+})
 
-function cleanup(){
+function cleanup () {
   try {
-    client.close();
+    client.close()
   } catch (e) {
-    logger.error(e);
+    logger.error(e)
   }
 }
--- test.js
+++ test.js
@@ -1,26 +1,26 @@
-const logger = require('./logger');
+const logger = require('./logger')
 
 const myObject = {
-  name: "Tim",
-  lastname: "Apple",
+  name: 'Tim',
+  lastname: 'Apple',
   age: 88,
   children: [
-    "Jim",
-    "Jill"
+    'Jim',
+    'Jill'
   ]
 }
 
-logger.info("text info", {meta1: 'meta1'});
-logger.info("text info", myObject, {test2: 'test2'}, "finalstring");
-logger.error("text error", myObject, {test2: 'test2'}, "finalstring");
-logger.info("text info", "second part of info");
-logger.info("text info --> null", null);
-logger.warn("text warn");
-logger.error("text error");
-logger.error("text error", "i am more error info");
-logger.debug("text debug");
-logger.debug("text verbose");
-logger.error(new Error("something went wrong"));
-logger.error("text error");
-logger.error("text error with object", myObject);
-logger.error(myObject);
+logger.info('text info', { meta1: 'meta1' })
+logger.info('text info', myObject, { test2: 'test2' }, 'finalstring')
+logger.error('text error', myObject, { test2: 'test2' }, 'finalstring')
+logger.info('text info', 'second part of info')
+logger.info('text info --> null', null)
+logger.warn('text warn')
+logger.error('text error')
+logger.error('text error', 'i am more error info')
+logger.debug('text debug')
+logger.debug('text verbose')
+logger.error(new Error('something went wrong'))
+logger.error('text error')
+logger.error('text error with object', myObject)
+logger.error(myObject)
